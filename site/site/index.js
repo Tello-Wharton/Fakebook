c9cdc843c6039a02d4c9130ad921ee9593c6dce1
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-03-11 21:09:22 +0000

    added weighting for id

--- site/site/index.js
+++ site/site/index.js
@@ -7,6 +7,8 @@ var client = algoliasearch('TRSE7O3IIG', '883a415024ea2e75c8451da21a42d077');
 
 client.deleteIndex("testing-purposes");
 
+var id_weight = "SuperAwesomeMakeAmericaGreatAganAndGetMyId";
+
 var index = client.initIndex('testing-purposes');
 
 app.use(express.static(__dirname + '/')); // set the static files location /public/img will be /img for users
@@ -14,7 +16,7 @@ app.listen(8001);
 
 
 app.get("/profile", function(req, res) {
-    var id = req.query.id;
+    var id = id_weight + req.query.id;
 
     index.search(id, function searchDone(err, content) {
       console.log(err, content);
@@ -26,12 +28,12 @@ app.get("/profile", function(req, res) {
 function algoliatest(){
 
   var objects = [{
-    id : "cake",
+    id : id_weight + "cake",
     firstname : "Aaron",
     surname : "Tello-Wharton",
     profile_pic : "http://ichef-1.bbci.co.uk/news/660/cpsprodpb/14C52/production/_92847058_c3c1256f-1f69-45fe-ade5-a1822e3d9b9c.jpg"
   }, {
-    id : "huel",
+    id : id_weight + "huel",
     firstname: 'Daddy',
     lastname: 'Lowe',
     profile_pic : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Kanadagans_Branta_canadensis.jpg/220px-Kanadagans_Branta_canadensis.jpg"
