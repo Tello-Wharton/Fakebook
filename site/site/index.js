81a78015340e4b1c5d546bee00ef0f0e1f16e66f
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-03-11 20:01:18 +0000

    corrected index loading

--- site/site/index.js
+++ site/site/index.js
@@ -4,11 +4,26 @@ var app = express();
 var algoliasearch = require("algoliasearch")
 var client = algoliasearch('TRSE7O3IIG', '883a415024ea2e75c8451da21a42d077');
 
+
+client.deleteIndex("testing-purposes");
+
+var index = client.initIndex('testing-purposes');
+
 app.use(express.static(__dirname + '/')); // set the static files location /public/img will be /img for users
 app.listen(8001);
 
+
+app.get("/profile", function(req, res) {
+    var id = req.query.id;
+
+    index.search(id, function searchDone(err, content) {
+      console.log(err, content);
+      res.send(JSON.stringify(content.hits[0]));
+    });
+});
+
+
 function algoliatest(){
-  var index = client.initIndex('testing-purposes');
 
   var objects = [{
     id : "cake",
@@ -30,10 +45,6 @@ function algoliatest(){
   index.search('Daddy', function searchDone(err, content) {
     console.log(err, content);
   });
-
-
-  client.deleteIndex("testing-purposes");
-
 
 }
 
